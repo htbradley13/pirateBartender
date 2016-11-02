972f750a3cee4a2420f5d1b0e9e9fa995d2cf647
htbradley13 <htbradley13@gmail.com>
2016-11-01 20:52:12 -0400

    Initial Commit, Matey

---
+++ app.js
@@ -0,0 +1,11 @@
+var Question = {
+	text: "", 
+	answerSelection: null 
+}
+
+var IngredientType = {
+	options: ""
+}
+
+// Add all IngredientType objects to this array once they are built
+var pantry = [];
